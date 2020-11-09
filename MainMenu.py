2e1a6b8116902331c8acf5be90ec005ef849391a
kajal-arthlearner <74099011+kajal-arthlearner@users.noreply.github.com>
2020-11-09 21:44:17 +0530

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -1,29 +1,31 @@
 from os import system
 
+
 while True:
-	while True:
-		system("clear")
-		print("\n\t"+48*"-")
-		print("\t\tAutomation Menu" by Arth Team-3.12)
-		print("\t"+48*"-")
-		print("\n\tNote: For the best experience, please ensure that yum is configured properly")
-		print("\n\tPlease select an option: \n")
-		system("tput setaf 14")
-		print("\t\t1 : AWS & Hadoop configuration")
-		print("\t\t2 : LVM configuration")
-		print("\t\t3 : Docker configuration")
-		print("\t\t0 : Exit")
-		
-		
-		if opt == '1':
-			os.system('python3 AWS & Hadoop configuration.py')
-			
-		elif opt == '2':
-    	                os.system('python3 lvm.py')
-			
-			
-		elif opt == '3':
-			os.system('python3 docker.py')	
-				
-		elif opt == '0':
-			exit()
+        while True:
+                system("clear")
+                print("\n\t"+48*"-")
+                print("\t\tAutomation Menu by Arth Team-3.12")
+                print("\t"+48*"-")
+                print("\n\tNote: For the best experience, please ensure that yum is configured properly")
+                print("\n\tPick one: \n")
+                system("tput setaf 14")
+                print("\t\t1 : AWS & Hadoop configuration")
+                print("\t\t2 : LVM configuration")
+                print("\t\t3 : Docker configuration")
+                print("\t\t0 : Exit")
+                opt=input()
+
+                if opt == '1':
+                        system('python3 'AWS & Hadoop configurations.py'')
+
+                elif opt == '2':
+                        system('python3 lvm.py')
+
+
+                elif opt == '3':
+                        system('python3 docker.py')
+
+                elif opt == '0':
+                        exit()
+~
